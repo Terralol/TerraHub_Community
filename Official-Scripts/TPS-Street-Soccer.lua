a0708b461011a7170363d987b9787a73113aa40f
TerraHub <dexhax76@gmail.com>
2025-12-16 15:50:27 +0300

    Update TPS-Street-Soccer.lua

--- Official-Scripts/TPS-Street-Soccer.lua
+++ Official-Scripts/TPS-Street-Soccer.lua
@@ -9,7 +9,7 @@
     TerraHub Community - Advanced Hub Script for Roblox Games
     MAP Support: TPS Street Soccer 
     Version: 1.1.1
-    Last Updated: 2025-12-06
+    Last Updated: 2025-12-30
     Telegaram: https://t.me/Terra_Hub
     Discord: discord.gg/FJzjGt6vH
 ]]
