70d4baa3bccfe67da3e0b977479c5a8e0ed1649b
TerraHub <dexhax76@gmail.com>
2025-12-16 16:06:08 +0300

    Update TPS-Street-Soccer.lua

--- Official-Scripts/TPS-Street-Soccer.lua
+++ Official-Scripts/TPS-Street-Soccer.lua
@@ -13,5 +13,6 @@
     Telegaram: https://t.me/Terra_Hub
     Discord: discord.gg/FJzjGt6vH
 ]]
+
 setclipboard("Telegram: https://t.me/Terra_Hub\nDiscord: https://discord.gg/FJzjGt6vH")
 game.Players.LocalPlayer:Kick("Script down. Check Telegram & Discord for updates.\n[Link Copied]")
