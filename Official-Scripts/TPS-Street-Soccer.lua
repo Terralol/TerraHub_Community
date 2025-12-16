d46baffbf28ca9e073e541c935d560f326028a01
TerraHub <dexhax76@gmail.com>
2025-12-16 18:52:45 +0300

    Update TPS-Street-Soccer.lua

--- Official-Scripts/TPS-Street-Soccer.lua
+++ Official-Scripts/TPS-Street-Soccer.lua
@@ -10,10 +10,10 @@
               Dedicated Map: TPS Street Soccer
               Version: 1.0.1
               Last Updated: 2025-12-30
-              Telegram: https://t.me/Terra_Hub
+              Telegram: t.me/Terra_Hub
               Discord: discord.gg/FJzjGt6vH
 ════════════════════════════════════════════════════════════════════════
 ]]
 
-setclipboard("Telegram: https://t.me/Terra_Hub\nDiscord: https://discord.gg/FJzjGt6vH")
+setclipboard("Telegram: t.me/Terra_Hub\nDiscord: discord.gg/FJzjGt6vH")
 game.Players.LocalPlayer:Kick("Script down. Check Telegram & Discord for updates.\n[Link Copied]")
