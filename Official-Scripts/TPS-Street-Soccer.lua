dbc877336e163f5708921f230388dc38ffaa6837
TerraHub <dexhax76@gmail.com>
2025-12-16 18:44:51 +0300

    Update TPS-Street-Soccer.lua

--- Official-Scripts/TPS-Street-Soccer.lua
+++ Official-Scripts/TPS-Street-Soccer.lua
@@ -1,18 +1,17 @@
---[[
 ════════════════════════════════════════════════════════════════════════
     ████████╗███████╗██████╗ ██████╗  █████╗ ██╗  ██╗██╗   ██╗██████╗ 
     ╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██║  ██║██║   ██║██╔══██╗
        ██║   █████╗  ██████╔╝██████╔╝███████║███████║██║   ██║██████╔╝
        ██║   ██╔══╝  ██╔══██╗██╔══██╗██╔══██║██╔══██║██║   ██║██╔══██╗
        ██║   ███████╗██║  ██║██║  ██║██║  ██║██║  ██║╚██████╔╝██████╔╝
-       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝ 
-    
-    TerraHub Community - Advanced Hub Script for Roblox Games
-    MAP Support: TPS Street Soccer 
-    Version: 1.0.1
-    Last Updated: 2025-12-30
-    Telegaram: https://t.me/Terra_Hub
-    Discord: discord.gg/FJzjGt6vH
+       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝
+
+              TerraHub Community - Advanced Hub Script for Roblox Games
+              Dedicated Map: TPS Street Soccer
+              Version: 1.0.1
+              Last Updated: 2025-12-30
+              Telegram: https://t.me/Terra_Hub
+              Discord: discord.gg/FJzjGt6vH
 ════════════════════════════════════════════════════════════════════════
 ]]
 
