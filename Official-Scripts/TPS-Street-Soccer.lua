9384974f2b7fa2e6e073cbb2d1ac1dbefabd2dc0
TerraHub <dexhax76@gmail.com>
2026-01-16 17:57:49 +0300

    Update TPS-Street-Soccer.lua

--- Official-Scripts/TPS-Street-Soccer.lua
+++ Official-Scripts/TPS-Street-Soccer.lua
@@ -12,9 +12,9 @@
               Version: 1.0.1
               Last Updated: 2025-12-30
               Telegram: t.me/Terra_Hub
-              Discord: discord.gg/FJzjGt6vH
+              Discord: https://discord.gg/aSzW3TDUX5
 ════════════════════════════════════════════════════════════════════════
 ]]
 
-setclipboard("Telegram: t.me/Terra_Hub\nDiscord: discord.gg/FJzjGt6vH")
+setclipboard("Telegram: t.me/Terra_Hub\nDiscord: https://discord.gg/aSzW3TDUX5")
 game.Players.LocalPlayer:Kick("Script down. Check Telegram & Discord for updates.\n[Link Copied]")
